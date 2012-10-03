cca96b27ab3d3c52aeab0df47169dea5bc2f6ed2
jramoyo <jramoyo@c29f06c5-f593-60b5-31af-ed673223a778>
2012-10-03 14:08:40 +0000

    Removing finished branch

--- branches/jramoyo-issue33/scripts/qfix-messenger-acceptor.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CLASSPATH=cfg/acceptor
-CLASSPATH=$CLASSPATH:lib/*
-
-java -cp "$CLASSPATH" com.jramoyo.qfixmessenger.QFixMessenger "cfg/acceptor/messenger.cfg" "cfg/acceptor/quickfix.cfg"
