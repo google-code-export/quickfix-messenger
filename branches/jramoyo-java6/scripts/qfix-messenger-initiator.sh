3619e4416c611eca36bbea020e89fe2e832207e2
jramoyo <jramoyo@c29f06c5-f593-60b5-31af-ed673223a778>
2012-10-10 15:47:12 +0000

    Deleting wrong branch

--- branches/jramoyo-java6/scripts/qfix-messenger-initiator.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CLASSPATH=cfg/initiator
-CLASSPATH=$CLASSPATH:lib/*
-
-java -cp "$CLASSPATH" com.jramoyo.qfixmessenger.QFixMessenger "cfg/initiator/messenger.cfg" "cfg/initiator/quickfix.cfg"
